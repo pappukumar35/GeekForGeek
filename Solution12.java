e9f68b7d0cf1631b815ec6354b75e65e97ef646e
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-23 12:38:03 +0530

    Add files via upload

---
+++ Solution12.java
@@ -0,0 +1,12 @@
+class Solution12 {
+    public boolean isPowerOfFour(int n) {
+        if (n == 0)
+            return false;
+        if (n == 1)
+            return true;
+        if (n % 4 != 0)
+            return false;
+
+        return isPowerOfFour(n / 4);
+    }
+}
