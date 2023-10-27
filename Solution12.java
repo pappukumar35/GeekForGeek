0aeffcf94bef0e0ad4a1ff368cb03dfbb59366ce
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-27 18:09:00 +0530

    Add files via upload

--- Solution12.java
+++ Solution12.java
@@ -1,12 +1,22 @@
 class Solution12 {
-    public boolean isPowerOfFour(int n) {
-        if (n == 0)
-            return false;
-        if (n == 1)
-            return true;
-        if (n % 4 != 0)
-            return false;
+    static int minimumNumberOfDeletions(String S) {
 
-        return isPowerOfFour(n / 4);
+        // code here
+        Integer[][] dp = new Integer[S.length()][S.length()];
+        return S.length() - lps(S, 0, S.length() - 1, dp);
+    }
+
+    public static int lps(String s, int start, int end, Integer[][] dp) {
+        if (start > end)
+            return 0;
+        if (start == end)
+            return 1;
+        if (dp[start][end] == null) {
+            if (s.charAt(start) == s.charAt(end))
+                dp[start][end] = 2 + lps(s, start + 1, end - 1, dp);
+            else
+                dp[start][end] = Math.max(lps(s, start + 1, end, dp), lps(s, start, end - 1, dp));
+        }
+        return dp[start][end];
     }
 }
