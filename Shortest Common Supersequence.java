5a264be932212293c8fe588994e4e690981e8812
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-11-13 09:55:29 +0530

    Add files via upload

---
+++ Shortest Common Supersequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int shortestCommonSupersequence(String x, String y, int n, int m) {
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return n + m - dp[n][m];
+    }
+}
