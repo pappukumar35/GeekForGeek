3a63c2f9d26b3778cdd467e710f300bff7745e09
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-12-23 09:41:49 +0530

    Add files via upload

---
+++ k_Occurences.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) {
+        k = n / k; // Updated value of k
+        int ans = 0;
+        Arrays.sort(arr);
+
+        for (int i = 1; i < n; i++) {
+            int count = 0;
+            while (i < n && arr[i] == arr[i - 1]) {
+                count++;
+                i++;
+            }
+            if (count >= k) ans++;
+        }
+        return ans;
+    }
+}
